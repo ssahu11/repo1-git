70cc7158dd5dbab51d880bb2ab154cb07251c2f7
ssahu11 <smrutisahu11@gmail.com>
2023-06-22 16:31:57 +0530

    Initial commit1

---
+++ sample-code-20230620T045753Z-001/sample-code/sample-code/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sample_code.MvcApplication" Language="C#" %>
---
+++ sample-code-20230620T045753Z-001/sample-code/sample-code/Models/Usercontroller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sample_code.Models
+{
+    public class Usercontroller
+    {
+
+        public int Number { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
